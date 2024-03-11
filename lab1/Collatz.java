690b035059f88fd5799c771a2fc6e46d97469689
Jingjing Pan <pan_jingjing@foxmail.com>
2024-03-11 20:49:48 +0800

    done with Collatz

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -1,17 +1,15 @@
 /** Class that prints the Collatz sequence starting from a given number.
- *  @author YOUR NAME HERE
+ *  @author Jingjing Pan
  */
 public class Collatz {
 
     /** Buggy implementation of nextNumber! */
     public static int nextNumber(int n) {
-        if (n  == 128) {
-            return 1;
-        } else if (n == 5) {
-            return 3 * n + 1;
-        } else {
-            return n * 2;
-        }
+      if (n % 2 != 0) {
+          return 3 * n + 1;
+      } else {
+          return n / 2;
+      }
     }
 
     public static void main(String[] args) {
@@ -21,7 +19,6 @@ public class Collatz {
             n = nextNumber(n);
             System.out.print(n + " ");
         }
-        System.out.println();
     }
 }
 
